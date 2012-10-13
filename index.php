7dde69415a80dfa4fb2af7d7e01b7dfc722b4281
Roberto B <roberto.butti@gmail.com>
2012-10-13 09:46:22 +0200

    namespace

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
  
 require 'slim/Slim/Slim.php';
-$app = new \Slim\Slim();
+
+$app = new Slim\Slim();
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
 });
