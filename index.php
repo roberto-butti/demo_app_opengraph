03290d608c1ef37a264941db7974cd9d11490886
Roberto B <roberto.butti@gmail.com>
2012-10-13 10:28:02 +0200

    info

--- index.php
+++ index.php
@@ -12,5 +12,7 @@ $app->get('/hello/:name', function ($name) {
 $app->get('/', function ($name = "Demo app Open graph") {
     echo "Hello, $name";
 });
-
+$app->get('/info', function () {
+    phpinfo();;
+});
 $app->run();
