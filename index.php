1e58ee9b2ed4adb74a203b2fb9dda817429588a5
Roberto B <roberto.butti@gmail.com>
2012-10-13 09:52:40 +0200

    autoloader slim

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
  
 require 'slim/Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
 
 $app = new Slim\Slim();
 $app->get('/hello/:name', function ($name) {
